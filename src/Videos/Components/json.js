e7a8ca938fdca0afcf9b68892d362399cc9cd9b6
Moonke7 <marcoscarreno78@gmail.com>
2024-07-08 18:28:44 -0400

    first commit

---
+++ src/Videos/Components/json.js
@@ -0,0 +1,62 @@
+export let json = [
+  {
+    src: "Videos/video1.mp4",
+  },
+  {
+    src: "Videos/video2.mp4",
+  },
+  {
+    src: "Videos/video3.mp4",
+  },
+  {
+    src: "Videos/video4.mp4",
+  },
+  {
+    src: "Videos/video5.mp4",
+  },
+  {
+    src: "Videos/video6.mp4",
+  },
+  {
+    src: "Videos/video7.mp4",
+  },
+  {
+    src: "Videos/video8.mp4",
+  },
+  {
+    src: "Videos/video9.mp4",
+  },
+  {
+    src: "Videos/video10.mp4",
+  },
+  {
+    src: "Videos/video11.mp4",
+  },
+  {
+    src: "Videos/video12.mp4",
+  },
+  {
+    src: "Videos/video13.mp4",
+  },
+  {
+    src: "Videos/video14.mp4",
+  },
+  {
+    src: "Videos/video15.mp4",
+  },
+  {
+    src: "Videos/video16.mp4",
+  },
+  {
+    src: "Videos/video17.mp4",
+  },
+  {
+    src: "Videos/video18.mp4",
+  },
+  {
+    src: "Videos/video19.mp4",
+  },
+  {
+    src: "Videos/video20.mp4",
+  },
+];
